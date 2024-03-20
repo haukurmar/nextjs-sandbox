30ad4932d7c07b42757a1faaf12fb0f593ab2ff1
Haukur Mar Bodvarsson <haukur.bodvarsson@nordax.se>
2024-03-20 23:04:32 +0100

    fix(useTranslation): added default value for namespace to be common

--- packages/locale/hooks/useTranslation.ts
+++ packages/locale/hooks/useTranslation.ts
@@ -1,6 +1,6 @@
 "use client";
 import { useTranslation as useTranslationBase } from "react-i18next";
 
-export const useTranslation = (namespaces: string | string[]) => {
+export const useTranslation = (namespaces: string | string[] = "common") => {
 	return useTranslationBase(namespaces);
 };
