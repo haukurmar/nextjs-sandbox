8e1594d886d919456be189849dcd93bfa7f392da
Haukur Mar Bodvarsson <haukurmar@gmail.com>
2024-04-18 16:51:41 +0200

    fix(config-release-it): pass in from basefile

--- packages/config-release-it/.release-it-prerelease.js
+++ packages/config-release-it/.release-it-prerelease.js
@@ -1,13 +1,32 @@
 const { execSync } = require("child_process");
-
+const baseFile = require("@app/config-release-it");
+const packageName = process.env.npm_package_name;
 const getPreReleaseTag = () => {
 	const branchName = execSync("git rev-parse --abbrev-ref HEAD").toString().trim();
 	// Replace slashes and other special characters with hyphens
 	return branchName.replace(/\/|[^a-zA-Z0-9]/g, "-");
 };
 
+const preRelease = getPreReleaseTag();
+
+console.log("baseFile", baseFile);
+
 module.exports = {
-	extends: "./.release-it.js",
-	requireBranch: ["beta", "next", "feature/*", "fix/*"],
-	preRelease: getPreReleaseTag(),
+	plugins: { ...baseFile.plugins },
+	preRelease: preRelease,
+	git: {
+		...baseFile.git,
+		tagName: `${packageName}-v${preRelease}`,
+		requireBranch: ["beta", "next", "feature/*", "fix/*"],
+	},
+	npm: {
+		...baseFile.npm,
+	},
+	github: {
+		...baseFile.github,
+		releaseName: `${packageName}-v${preRelease}`,
+	},
+	hooks: {
+		...baseFile.hooks,
+	},
 };
